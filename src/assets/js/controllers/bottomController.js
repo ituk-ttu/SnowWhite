ae3f92825f0b44da6d33ff131a622946e752524a
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2017-07-12 23:35:35 +0300

    Implemented card looping

--- src/assets/js/controllers/bottomController.js
+++ src/assets/js/controllers/bottomController.js
@@ -1,4 +1,32 @@
-app.controller("bottomController", ["$q", "$scope", "$http",
-    function ($q, $scope, $http) {
+app.controller("bottomController", ["$q", "$scope", "$http", "$interval",
+    function ($q, $scope, $http, $interval) {
+
+        var UPDATE_DELAY = 10 * 1000;
+
+        var CARDS = [
+        "busSchedule",
+        "events"
+        ];
+
+        var counter = 1;
+        var current = CARDS[0];
+        var previous = CARDS[1];
+
+        $scope.getClassForCard = function (cardName) {
+            return current === cardName ? "current" :
+                   previous === cardName ? "previous" :
+                   "";
+        };
+
+        var updateCurrentCard = function () {
+            previous = "";
+            $scope.$$postDigest(function() {
+                previous = current;
+                current = CARDS[counter % CARDS.length];
+                counter++;
+            });
+        };
+
+        $interval(updateCurrentCard, UPDATE_DELAY);
 
     }]);
