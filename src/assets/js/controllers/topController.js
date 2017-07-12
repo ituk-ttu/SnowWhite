a99c5f5488c8d8a7940e222506397ea036d2f4f9
Johannes Kümmel <jkymmel@gmail.com>
2017-07-12 15:03:42 +0300

    Added clock and date

--- src/assets/js/controllers/topController.js
+++ src/assets/js/controllers/topController.js
@@ -1,4 +1,8 @@
-app.controller("topController", ["$q", "$scope", "$http",
-    function ($q, $scope, $http) {
-
+app.controller("topController", ["$q", "$scope", "$http", "$interval",
+    function ($q, $scope, $http, $interval) {
+        var tick = function() {
+            $scope.clock = Date.now();
+        };
+        tick();
+        $interval(tick, 1000);
     }]);
