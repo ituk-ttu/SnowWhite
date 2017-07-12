abc9706e65c95da3f76fb50ab99ea64eda5aac7a
Johannes Kümmel <jkymmel@gmail.com>
2017-07-12 17:09:04 +0300

    Added weather icons (Climacons)

--- src/assets/js/controllers/topController.js
+++ src/assets/js/controllers/topController.js
@@ -6,6 +6,27 @@ app.controller("topController", ["$q", "$scope", "$http", "$interval",
 
         $scope.weather = null;
 
+        var icons = {
+            "01d": "Sun",
+            "01n": "Moon",
+            "02d": "Cloud-Sun",
+            "02n": "Cloud-Moon",
+            "03d": "Cloud",
+            "03n": "Cloud",
+            "04d": "Cloud",
+            "04n": "Cloud",
+            "09d": "Cloud-Rain",
+            "09n": "Cloud-Rain",
+            "10d": "Cloud-Rain-Sun",
+            "10n": "Cloud-Rain-Moon",
+            "11d": "Cloud-Lightning",
+            "11n": "Cloud-Lightning",
+            "13d": "Cloud-Snow",
+            "13n": "Cloud-Snow",
+            "50d": "Cloud-Fog",
+            "50n": "Cloud-Fog"
+        };
+
         var tick = function () {
             $scope.clock = Date.now();
         };
@@ -24,7 +45,7 @@ app.controller("topController", ["$q", "$scope", "$http", "$interval",
             ).then(function (result) {
                 $scope.weather = {
                     temperature: result.data.main.temp,
-                    sky: result.data.weather[0].main
+                    icon: icons[result.data.weather[0].icon]
                 }
             });
         };
