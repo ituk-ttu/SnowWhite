a680500377bc79ab6700c685699962c3acbfd69a
Johannes Kümmel <jkymmel@gmail.com>
2017-09-19 09:40:22 +0300

    Updated calendar ID

--- src/assets/js/controllers/eventsController.js
+++ src/assets/js/controllers/eventsController.js
@@ -3,7 +3,7 @@ app.controller("eventsController", ["$q", "$scope", "$http", "$interval",
 
         var self = this;
 
-        var CALENDAR_ID = "sflecuo8b8t3rv7vn35rni8nfc@group.calendar.google.com";
+        var CALENDAR_ID = "g86lrthmecu19gh7arvcj76f08@group.calendar.google.com";
         var UPDATE_DELAY = 60 * 60 * 1000;
 
         $scope.events = null;
